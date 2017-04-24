f22f1166251ff4e5a9a9b80ee6e5a4dabd7d9876
sviatkin <yariks@i.ua>
2017-04-24 13:02:42 +0300

    add logger

--- src/test/java/Tests/MyTest.java
+++ src/test/java/Tests/MyTest.java
@@ -1,6 +1,7 @@
 package Tests;
 
 import Pages.LoginPage;
+import org.apache.log4j.Logger;
 import org.junit.AfterClass;
 import org.junit.rules.Timeout;
 import org.openqa.selenium.WebDriver;
@@ -15,6 +16,7 @@ import org.testng.annotations.Test;
  * Created by DELL on 3/26/2017.
  */
 public class MyTest extends BaseTest {
+    Logger logger = Logger.getLogger("MYTest");
     LoginPage loginPage;
     @BeforeClass
     public void beforeClassMyTest() throws Exception {
@@ -25,7 +27,9 @@ public class MyTest extends BaseTest {
     @Test(description = "Login Test")
     public void testTest() throws Exception {
 
+
       String actual = loginPage.login("mytest_test","337774a").getMailUser();
+      logger.info("actual - " + actual);
 
        Assert.assertEquals(actual,"mytest_test@mail.ru","Error ");
 
