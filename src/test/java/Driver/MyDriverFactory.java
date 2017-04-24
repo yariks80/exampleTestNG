3d3748ea7165769f3e368857a0608f3ecf9cefa0
sviatkin <yariks@i.ua>
2017-04-24 13:14:00 +0300

    add driver factory

--- src/test/java/Driver/MyDriverFactory.java
+++ src/test/java/Driver/MyDriverFactory.java
@@ -2,7 +2,11 @@ package Driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by DELL on 4/9/2017.
@@ -11,14 +15,37 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class MyDriverFactory {
     public static WebDriver getDriver() {
         String property = System.getProperty("driver");
-        switch (property) {
-            case "chrome":
-                System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
-                return new ChromeDriver();
+        WebDriver driver = null;
+        String nameDriver = System.getProperty("driver");
+        if (nameDriver != null) {
+            switch (nameDriver) {
+                case "chrome":
+                    System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    System.setProperty("webdriver.gecko.driver", "drivers\\geckodriver.exe");
+                    driver = new FirefoxDriver();
+                    break;
+                case "ie":
+                    System.setProperty("webdriver.ie.driver", "drivers\\IEDriverServer.exe");
+                    driver = new InternetExplorerDriver();
+                    break;
+                case "edge":
+                    System.setProperty("webdriver.edge.driver", "drivers\\MicrosoftWebDriver.exe");
+                    driver = new EdgeDriver();
+                    break;
 
-            default:
-                System.setProperty("webdriver.gecko.driver", "drivers\\geckodriver.exe");
-                return new FirefoxDriver();
+            }
+        } else {
+
+            System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+            driver = new ChromeDriver();
         }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return driver;
     }
+
 }
+
