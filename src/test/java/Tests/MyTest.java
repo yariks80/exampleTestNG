4528db8b28b962dfd3218ad4da7a51eabce6f300
sviatkin <yariks@i.ua>
2017-04-29 07:46:04 +0300

    add driver factory

--- src/test/java/Tests/MyTest.java
+++ src/test/java/Tests/MyTest.java
@@ -2,19 +2,17 @@ package Tests;
 
 import Pages.LoginPage;
 import org.apache.log4j.Logger;
-import org.junit.AfterClass;
-import org.junit.rules.Timeout;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Title;
 
 /**
  * Created by DELL on 3/26/2017.
  */
+
+@Description("This is an example test suite")
 public class MyTest extends BaseTest {
     Logger logger = Logger.getLogger("MYTest");
     LoginPage loginPage;
@@ -23,7 +21,7 @@ public class MyTest extends BaseTest {
         loginPage= new LoginPage(driver);
 
     }
-
+    @Title("Failing test")
     @Test(description = "Login Test")
     public void testTest() throws Exception {
 
